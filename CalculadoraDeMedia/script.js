78ac8f6d3b58248863caf1f9cc21df36266a303f
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-03-07 21:53:44 -0300

    ImersãoDev22 - First Commit

---
+++ CalculadoraDeMedia/script.js
@@ -0,0 +1,17 @@
+let name = "Daniel";
+let notaDoPrimeiroBimestre = 9.256;
+let notaDoSegundoBimestre = 7.323;
+let notaDoTerceiroBimestre = 4.445;
+let notaDoQuartoBimestre = 2.878;
+
+let notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4;
+
+let notaFixada = notaFinal.toFixed(1);
+
+console.log(notaFixada);
+
+
+
+
+
+
