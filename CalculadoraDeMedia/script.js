9e8b613c0f8470b1693ad02be8c78d94d3432bea
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-03-07 22:33:41 -0300

    ImersãoDev22 - condicional

--- CalculadoraDeMedia/script.js
+++ CalculadoraDeMedia/script.js
@@ -4,13 +4,22 @@ let notaDoSegundoBimestre = 7.323;
 let notaDoTerceiroBimestre = 4.445;
 let notaDoQuartoBimestre = 2.878;
 
-let notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4;
+let notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre 
+    + notaDoQuartoBimestre) / 4;
 
 let notaFixada = notaFinal.toFixed(1);
 
-console.log(notaFixada);
 
-
+    if (notaFixada > 7.0) {
+        console.log("A sua nota foi " + notaFixada + " e você foi aprovado");
+        
+    } else {
+        console.log("Estude mais! A sua nota foi " + notaFixada + " e você foi reprovado")
+      
+    };   
+      
+//console.log(notaFixada);
+      
 
 
 
